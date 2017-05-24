93582845aa7debcd0412440113014c5bb2096c24
patterncoder <patterncoder@gmail.com>
2017-05-23 21:10:27 -0700

    update user schema

--- account/UserSchema.js
+++ account/UserSchema.js
@@ -6,8 +6,8 @@ var sharedSchema = require('../sharedSchemas.js');
 
 var userSchema = new mongoose.Schema({
     meta: sharedSchema.metaSchema,
-    firstName: {type:String, required:"{PATH} is required!"},
-    lastName: {type:String, required:"{PATH} is required!"},
+    firstName: {type:String, required:"{PATH} is required!", caption: "First Name"},
+    lastName: {type:String, required:"{PATH} is required!", caption: "First Name"},
     username: sharedSchema.uniqueEmail.email,
     // {
     //     type: String,
@@ -16,8 +16,8 @@ var userSchema = new mongoose.Schema({
     // },
     company: {type:mongoose.Schema.Types.ObjectId, ref:'Company'},
     //adding security fields here
-    salt: {type:String, required:"{PATH} is required!"},
-    hashed_pwd: {type:String, required:"{PATH} is required!"},
+    salt: {type:String},
+    hashed_pwd: {type:String},
     roles: [{type:String, enum: ['superUser', 'admin', 'gold', 'silver', 'bronze']}]
 
 });
