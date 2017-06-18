38d626ddae2caefc0312c5d7883e133012fb1cb4
crs2here <crs2here@gmail.com>
2017-06-18 11:42:43 -0700

    add roles to lookups schema

--- common/LookupsSchema.js
+++ common/LookupsSchema.js
@@ -4,9 +4,8 @@ var sharedSchema = require('../sharedSchemas');
 var lookupsSchema = new mongoose.Schema({
     meta: sharedSchema.metaSchema,
     menuItemTags: [String],
-    contactTags: [String]
-
-
+    contactTags: [String],
+    roles:[String]
 });
 
 module.exports = lookupsSchema;
