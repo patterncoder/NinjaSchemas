825b29f591154096762ebf5244227ca5db7d3912
trillobite <jparnell0@protonmail.com>
2021-06-09 14:41:39 -0700

    added a quantity to rental items

--- events/RentalItemSchema.js
+++ events/RentalItemSchema.js
@@ -3,17 +3,22 @@ var sharedSchemas = require('../sharedSchemas');
 
 var rentalItemSchema = mongoose.Schema({
     meta: sharedSchemas.metaSchema,
-    name: {type: String,
+    name: {
+        type: String,
         required: 'The rental item name is required',
         //minlength: [8, 'The rental item name must be at least 8 characters'], 
         caption: 'Rental Item Name',
-        tabOrder: 10},
+        tabOrder: 10
+    },
     // description: {type: String,
     //     required: 'The menu description is required',
     //     caption: 'Description',
     //     tabOrder: 20},
-    price: {type:Number,
-    	min: 0},
+    price: {
+        type:Number,
+    	min: 0
+    },
+    quantity: Number,
     inHouse: Boolean
 
 });
