604d9a28d1741b7a01e765995fe5b36c93da5bbe
trillobite <jparnell0@protonmail.com>
2021-08-04 22:16:04 -0700

    added schema for settings

---
+++ account/Settings.js
@@ -0,0 +1,42 @@
+
+var mongoose = require('mongoose');
+
+var sharedSchema = require('../sharedSchemas.js');
+
+
+var settingsSchema = new mongoose.Schema({
+    meta: sharedSchema.metaSchema,
+    tax: {
+        type: Number,
+        required: "Must have a tax value.",
+        caption: "Tax",
+        tabOrder: 10
+    },
+    defaultDeposit: {
+        type: Number,
+        caption: "Default Deposit",
+        tabOrder: 20
+    },
+    minGratuity: {
+        type: Number,
+        caption: "Minimum Gratuity",
+        required: "Must have a minimum gratuity percentage",
+        tabOrder: 30
+    },
+    brandingLogo: {
+        type: Image,
+        caption: "Branding Logo",
+        required: false,
+        tabOrder: 40
+    }
+});
+
+
+
+
+module.exports = settingsSchema;
+
+
+
+
+
--- account/index.js
+++ account/index.js
@@ -1,3 +1,4 @@
 exports.Company = require('./CompanySchema');
 exports.User = require('./UserSchema');
 exports.Signup = require('./SignupSchema');
+exports.Settings = require("./Settings");
