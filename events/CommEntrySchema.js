f61b5b3c729e05f0c889e6312c64d94816f49a09
trillobite <jparnell0@protonmail.com>
2021-09-16 13:44:15 -0700

    added communications schema

---
+++ events/CommEntrySchema.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var sharedSchemas = require('../sharedSchemas');
+
+var CommEntrySchema = mongoose.Schema({
+    meta: sharedSchemas.metaSchema,
+    date: {
+        type: Date,
+        required: true,
+        caption: "Date is required to make a comm entry.",
+        tabOrder: 10
+    },
+    commType: {
+        type: String,
+        required: true,
+        caption: "We need to know what kind of comm this is.",
+        tabOrder: 20
+    },
+    employee: {
+        type: String,
+        required: true,
+        tabOrder: 30
+    },
+    description: {
+        type: String,
+        tabOrder: 40
+    }
+});
+
+module.exports = CommEntrySchema;
--- events/index.js
+++ events/index.js
@@ -2,3 +2,4 @@ exports.Contract = require('./ContractSchema');
 exports.RentalItem = require('./RentalItemSchema');
 exports.Venue = require('./VenueSchema');
 exports.StaffMember = require('./StaffMemberSchema');
+exports.CommEntry = require('./CommEntrySchema');
